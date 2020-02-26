77d77a8498c0ac1c68d162099216bb711bcb9dd5
prosper00 <57373245+prosper00@users.noreply.github.com>
2020-02-26 16:10:51 -0700

    Add files via upload

---
+++ grip.h
@@ -0,0 +1,33 @@
+/* Pin definitions. Use whatever pins suit your project, but
+   be mindful that the 'CLK' buttons need to be connected to a
+   pin with a hardware interrupt. This may vary depending
+   on the exact board model. I'm using a Leonardo */
+#define JS1CLKPin 2 // PD1, joystick 1 button 0, GRiP JS1 clock. Need an int 
+#define JS1DATPin 4 // PD4, joystick 1 button 1, GRiP JS1 data
+#define JS2CLKPin 3 // PD0, joystick 2 button 0, GRiP JS2 clock. Need an int
+#define JS2DATPin 6 // PD7, joystick 2 button 1, GRiP JS2 data
+#define RXLED 17 // The Leonardo RX LED
+#define TXLED 30 // The Leonardo TX LED
+#define GRIPSZ 24 // GRiP packetsize is 24 bytes
+
+/*  Data packet consists of 24 bytes, as follows:
+  0 1 1 1 1 1 0 Select Start R2 Blue 0 L2 Green Yellow Red 0 L1 R1 Up Down 0 Right Left
+  Bitmap to express
+ */
+#define JS_left   0b00000000000000001
+#define JS_right  0b00000000000000010
+        //framing 0b00000000000000100
+#define JS_down   0b00000000000001000
+#define JS_up     0b00000000000010000
+#define JS_r1     0b00000000000100000
+#define JS_l1     0b00000000001000000
+        //framing 0b00000000010000000
+#define JS_red    0b00000000100000000
+#define JS_yellow 0b00000001000000000
+#define JS_green  0b00000010000000000
+#define JS_l2     0b00000100000000000
+        //framing 0b00001000000000000
+#define JS_blue   0b00010000000000000
+#define JS_r2     0b00100000000000000
+#define JS_start  0b01000000000000000
+#define JS_select 0b10000000000000000
