f04ab4081d368a7ca0d6063e598e41a1dc8ab749
prosper00 <57373245+prosper00@users.noreply.github.com>
2020-02-28 15:06:28 -0700

    Update grip.h

--- grip.h
+++ grip.h
@@ -10,56 +10,44 @@
 #define TXLED 30 // The Leonardo TX LED
 #define GRIPSZ 24 // GRiP packetsize is 24 bytes
 
-/* Keymapping table: */
-//JS1 defs
-#define JS1_left   KEY_LEFT_ARROW
-#define JS1_right  KEY_RIGHT_ARROW
-#define JS1_up     KEY_UP_ARROW
-#define JS1_down   KEY_DOWN_ARROW
-#define JS1_start  KEY_RETURN
-#define JS1_select KEY_ESC
-#define JS1_red    KEY_RIGHT_SHIFT
-#define JS1_yellow ' '
-#define JS1_blue   KEY_RIGHT_ALT
-#define JS1_green  KEY_RIGHT_CTRL
-#define JS1_l1     'l'
-#define JS1_r1     ':'
-#define JS1_l2     '.'
-#define JS1_r2     '/'
+/* Keymapping table: 
+* Our compressed data packet consists of 2 bytes, as follows:
+* 0 0 Select Start R2 Blue L2 Green Yellow Red L1 R1 Up Down Right Left
+*/
+//JS1 keymap
+const char JS_keys[2][14] = {
+  {
+  KEY_LEFT_ARROW,  // JS1_left   bit 0000000000000001
+  KEY_RIGHT_ARROW, // JS1_right  bit 0000000000000010
+  KEY_DOWN_ARROW,  // JS1_down   bit 0000000000000100
+  KEY_UP_ARROW,    // JS1_up     bit 0000000000001000
+  ':',             // JS1_r1     bit 0000000000010000
+  'l',             // JS1_l1     bit 0000000000100000
+  KEY_RIGHT_SHIFT, // JS1_red    bit 0000000001000000
+  ' ',             // JS1_yellow bit 0000000010000000
+  KEY_RIGHT_CTRL,  // JS1_green  bit 0000000100000000
+  '.',             // JS1_l2     bit 0000001000000000
+  KEY_RIGHT_ALT,   // JS1_blue   bit 0000010000000000
+  '/',             // JS1_r2     bit 0000100000000000
+  KEY_RETURN,      // JS1_start  bit 0001000000000000
+  KEY_ESC          // JS1_select bit 0010000000000000
+  },
 
-//JS2 defs
-#define JS2_left   'a'
-#define JS2_right  'd'
-#define JS2_up     'w'
-#define JS2_down   's'
-#define JS2_start  'z'
-#define JS2_select 'c'
-#define JS2_red    'r'
-#define JS2_yellow 'f'
-#define JS2_blue   't'
-#define JS2_green  'g'
-#define JS2_l1     'q'
-#define JS2_r1     'e'
-#define JS2_l2     '1'
-#define JS2_r2     '3'
-
-/* Our compressed data packet consists of 2 bytes, as follows:
-  B1: Yellow Red L1 R1 Up Down Right Left
-  B1: 0 0 Select Start R2 Blue L2 Green
-  Bitmap to express:
- */
-#define DECODE_left   0b00000001
-#define DECODE_right  0b00000010
-#define DECODE_down   0b00000100
-#define DECODE_up     0b00001000
-#define DECODE_r1     0b00010000
-#define DECODE_l1     0b00100000
-#define DECODE_red    0b01000000
-#define DECODE_yellow 0b10000000
-
-#define DECODE_green  0b00000001
-#define DECODE_l2     0b00000010
-#define DECODE_blue   0b00000100
-#define DECODE_r2     0b00001000
-#define DECODE_start  0b00010000
-#define DECODE_select 0b00100000
+//JS2 keymap
+  {
+  'a', // JS2_left   bit 0000000000000001
+  'd', // JS2_right  bit 0000000000000010
+  's', // JS2_down   bit 0000000000000100
+  'w', // JS2_up     bit 0000000000001000
+  'e', // JS2_r1     bit 0000000000010000
+  'q', // JS2_l1     bit 0000000000100000
+  'r', // JS2_red    bit 0000000001000000
+  'f', // JS2_yellow bit 0000000010000000
+  'g', // JS2_green  bit 0000000100000000
+  '1', // JS2_l2     bit 0000001000000000
+  't', // JS2_blue   bit 0000010000000000
+  '3', // JS2_r2     bit 0000100000000000
+  'z', // JS2_start  bit 0001000000000000
+  'c'  // JS2_select bit 0010000000000000
+  }
+};
